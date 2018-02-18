a694c6dbfe7cb58a58e30564f3ca321a3870bff8
mihaivanea <mihaivanea1@gmail.com>
2018-02-18 17:36:24 +0000

    Implemented Acceptor. NB: be careful how you choose the ballot number allocation scheme.

--- lib/acceptor.ex
+++ lib/acceptor.ex
@@ -2,4 +2,25 @@
 
 defmodule Accpetor do
 
+  def start() do
+    ballot_num = 0
+    accepted = MapSet.new()
+    next(ballot_num, accepted)
+  end # start
+
+  defp next(ballot_num, accepted) do
+    receive do
+      {:p1a, leader, b} -> 
+        if b > ballot_num do
+          ballot_num = b
+        end
+        send(leader, {:p1b, self(), ballot_num, accepted})
+      {:p2a, leader, {b, s, c}} -> 
+        if b == ballot_num do
+          accepted = MapSet.put(accepted, {b, s, c})
+        end
+        send(leader, {:p2b, self(), ballot_num})
+    end
+  end # next
+
 end # Acceptor
