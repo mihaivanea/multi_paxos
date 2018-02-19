1d8c7a33481f5af5fbf18f891cd98126537c7853
mihaivanea <mihaivanea1@gmail.com>
2018-02-19 21:22:07 +0000

    Implemented replica.

--- lib/replica.ex
+++ lib/replica.ex
@@ -1,48 +1,45 @@
-# Mihail Vanea (mv1315)
+# Mihail Vanea (mv1316)
 
 defmodule Replica do
 
-  def start(leaders, initial_state) do
-    state = initial_state
-    slot_in = 1
-    slot_out = 1
-    requests = MapSet.new()
-    proposals = MapSet.new()
-    decisions = MapSet.new()
-    next(state, slot_in, slot_out, requests, proposals, decisions, leaders)
+  def start(_, database, _) do
+    receive do
+      {:bind, leaders} ->  
+        next(nil, 1, 1, MapSet.new(), MapSet.new(), MapSet.new(), leaders, database)
+    end
   end # start
 
-  defp next(state, slot_in, slot_out, requests, proposals, decisions, leaders) do
+  defp next(state, slot_in, slot_out, requests, proposals, decisions, leaders, database) do
     receive do
       {:request, c} -> 
         new_requests = MapSet.put(requests, c)
         {new_slot_in, new_requests, new_proposals} = 
           propose(slot_in, slot_out, new_requests, proposals, decisions, leaders)
-        next(state, new_slot_in, slot_out, new_requests, new_proposals, decisions, leaders)
+        next(state, new_slot_in, slot_out, new_requests, new_proposals, decisions, leaders, database)
       {:decision, s, c} -> 
         new_decisions = MapSet.put(decisions, {s, c})
         {new_proposals, new_requests, new_slot_out, new_state} = while_decision(
-          new_decisions, proposals, requests, slot_out, c, state, leaders)
+          new_decisions, proposals, requests, slot_out, c, state, leaders, database)
         {new_slot_in, new_requests, new_proposals} = 
           propose(slot_in, new_slot_out, new_requests, new_proposals, new_decisions, leaders)
-        next(new_state, new_slot_in, new_slot_out, new_requests, new_proposals, new_decisions, leaders)
+        next(new_state, new_slot_in, new_slot_out, new_requests, new_proposals, new_decisions, leaders, database)
     end
   end # next
 
-  defp while_decision(decisions, proposals, requests, slot_out, c, state, leaders) do
+  defp while_decision(decisions, proposals, requests, slot_out, c, state, leaders, database) do
     if DAC.there_exists(decisions, {slot_out, c}) do
       c_prime = for {^slot_out, c} <- MapSet.to_list(decisions), do: c
       {new_proposals, new_requests} = if DAC.there_exists(proposals, {slot_out, c}) do
           c_second = for {^slot_out, c} <- MapSet.to_list(proposals), do: c
           new_proposals = MapSet.delete(proposals, {slot_out, c_second})
-          new_requests = if c_second != c_prime do requests = MapSet.put(
+          new_requests = if c_second != c_prime do MapSet.put(
             requests, c_second) else requests end
           {new_proposals, new_requests}
         else
           {proposals, requests}
         end
-      {new_slot_out, new_state} = perform(c_prime, decisions, slot_out, state)
-      while_decision(decisions, new_proposals, new_requests, new_slot_out, c, new_state, leaders)
+      {new_slot_out, new_state} = perform(c_prime, decisions, slot_out, state, database)
+      while_decision(decisions, new_proposals, new_requests, new_slot_out, c, new_state, leaders, database)
     else
       {proposals, requests, slot_out, state}
     end
@@ -53,21 +50,24 @@ defmodule Replica do
 
   end # propose
 
-  defp perform({k, cid, op}, decisions, slot_out, state) do
-    slots = for {s, c} <- decisions, do: s
+  defp perform({_, _, op}, decisions, slot_out, state, database) do
+    slots = for {s, _} <- decisions, do: s
     # TODO: reconfig() for crash tolerance
     if Enum.any?(slots, fn(s) -> s < slot_out end) do
       {slot_out + 1, state}
     else
-      {next, result} = op(state) 
-      send()
-      send(k, {:response, cid, result})
-      {slot_out + 1, next}
+      # TODO: for crash tolerance
+      # {next, result} = op(state) 
+      send(database, {:execute, op})
+      # TODO: for crash tolerance
+      # send(k, {:response, cid, result})
+      {slot_out + 1, state}
     end
   end # perform
 
   defp while_propose(slot_in, slot_out, requests, proposals, decisions, leaders) do
-    if slot_in < slot_out + window and Enum.empty?(requests)do
+    #if slot_in < slot_out + window and Enum.empty?(requests)do
+    if slot_in < slot_out + 3 and Enum.empty?(requests)do
       # if statement for fault tolerance
       c = Enum.at(requests, 0) 
       {new_requests, new_proposals} = 
